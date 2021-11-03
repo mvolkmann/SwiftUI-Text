80d29ab64f90ce29dca1c1995c08ae6907015e19
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-03 16:12:12 -0500

    Initial Commit

---
+++ SwiftUI-Text/SwiftUI_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TextApp.swift
+//  SwiftUI-Text
+//
+//  Created by R. Mark Volkmann on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
